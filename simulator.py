058aa9defa5e94cd862e28db692781b9862c7b1e
Mirko <mirkoletto@gmail.com>
2019-11-28 23:42:48 +0100

    Following naming convention

--- simulator.py
+++ simulator.py
@@ -13,28 +13,31 @@ TARGET = 1
 
 
 class Simulator:
-    def __init__(self, space_shape, ntargets, nwalls):
-        self.render_space_shape = (1000, 1000)
-        self.ntargets = ntargets
-        self.nwalls = nwalls
+    def __init__(self, space_shape, batch_size, agent_size, ntargets, nwalls, rendering):
+        self.__render_space_shape = (1000, 1000)
+        self.__batch_size = batch_size
+        self.__agent_size = agent_size
+        self.__ntargets = ntargets
+        self.__nwalls = nwalls
+        self.__rendering = rendering
         #Max/min wall length is a percentage of space height
-        self.max_wall_len = 0.7
-        self.min_wall_len = 0.3
+        self.__max_wall_len = 0.7
+        self.__min_wall_len = 0.3
 
-        self.w = space_shape[0]
-        self.h = space_shape[1]
+        self.__w = space_shape[0]
+        self.__h = space_shape[1]
 
         #How much is the step in the render space
-        self.dw = self.render_space_shape[0]/self.w
-        self.dh = self.render_space_shape[1]/self.h
+        self.__dw = self.__render_space_shape[0]/self.__w
+        self.__dh = self.__render_space_shape[1]/self.__h
 
-        self.screen = pygame.display.set_mode(self.render_space_shape)
-        self.map = np.zeros((self.w, self.h))
+        self.__screen = pygame.display.set_mode(self.__render_space_shape)
+        self.__map = np.zeros((self.__w, self.__h))
 
-        self._generateWalls()
-        self._generateTargets()
+        self.__generate_walls()
+        self.__generate_targets()
 
-    def _generateWalls(self):
+    def __generate_walls(self):
         #We generate nwalls based on the x, with a random lenght
         #This has to be done BEFORE picking the targets
         #That's not the best method and it is NOT guarenteed to work
@@ -42,72 +45,73 @@ class Simulator:
         #Ex. with high number of walls
         # THIS HAS TO BE EDITED
         i = 0
-        while i < self.nwalls:
-            x = int(np.random.rand()*self.w)
-            if self.map[x, 0] == WALL or self.map[x, -1] == WALL:
+        while i < self.__nwalls:
+            x = int(np.random.rand()*self.__w)
+            if self.__map[x, 0] == WALL or self.__map[x, -1] == WALL:
                 continue
 
-            wall_lenght = np.random.rand()*self.h*(self.max_wall_len-self.min_wall_len)
-            wall_lenght += self.min_wall_len*self.h
+            wall_lenght = np.random.rand()*self.__h*(self.__max_wall_len-self.__min_wall_len)
+            wall_lenght += self.__min_wall_len*self.__h
             wall_lenght = int(wall_lenght)
-            wall_arr = np.full(self.h, False)
+            wall_arr = np.full(self.__h, False)
             wall_arr[:wall_lenght] = True
             reverse = np.random.rand() < 0.5
-            self.map[x, wall_arr if not reverse else ~wall_arr] = WALL
+            self.__map[x, wall_arr if not reverse else ~wall_arr] = WALL
 
             #Check if a path is possible
             #Doesn't work properly
-            sum = self.map[x, wall_arr]
+            sum = self.__map[x, wall_arr]
             if x != 0:
-                sum += self.map[x-1, wall_arr]
-            if x != self.w-1:
-                sum += self.map[x+1, wall_arr]
+                sum += self.__map[x-1, wall_arr]
+            if x != self.__w-1:
+                sum += self.__map[x+1, wall_arr]
                 
             sum = np.max(sum)
             if sum > WALL:
-                self.map[x, wall_arr if not reverse else ~wall_arr] = 0
+                self.__map[x, wall_arr if not reverse else ~wall_arr] = 0
                 continue 
 
             i+=1
 
-    def _generateTargets(self):
+    def __generate_targets(self):
         #We pick ntargets points in the space
-        self.targets_mask = np.full((self.w, self.h), False)
+        self.__targets_mask = np.full((self.__w, self.__h), False)
         i = 0
-        while i < self.ntargets:
-            x = int(np.random.rand()*self.w)
-            y = int(np.random.rand()*self.h)
-            if self.targets_mask[x, y] == False and self.map[x, y] == 0:
-                self.targets_mask[x, y] = True
+        while i < self.__ntargets:
+            x = int(np.random.rand()*self.__w)
+            y = int(np.random.rand()*self.__h)
+            if self.__targets_mask[x, y] == False and self.__map[x, y] == 0:
+                self.__targets_mask[x, y] = True
             else:
                 #We have to retry because we extracted the same target before
                 continue
             i+=1
-        self.map[self.targets_mask] = TARGET
+        self.__map[self.__targets_mask] = TARGET
 
     def render(self):
-        self.screen.fill(BG)
-        for x in range(self.w):
-            for y in range(self.h):
-                if self.map[x, y] == WALL:
-                    rect = pygame.Rect(self.dw*x, self.dh*y, self.dw, self.dh)
-                    pygame.draw.rect(self.screen, WALL_COLOR, rect)
+        if self.__rendering:
+            self.__screen.fill(BG)
+            for x in range(self.__w):
+                for y in range(self.__h):
+                    if self.__map[x, y] == WALL:
+                        rect = pygame.Rect(self.__dw*x, self.__dh*y, self.__dw, self.__dh)
+                        pygame.draw.rect(self.__screen, WALL_COLOR, rect)
 
-                if self.map[x, y] == TARGET:
-                    rect = pygame.Rect(self.dw*x, self.dh*y, self.dw, self.dh)
-                    pygame.draw.rect(self.screen, TARGET_COLOR, rect)
+                    if self.__map[x, y] == TARGET:
+                        rect = pygame.Rect(self.__dw*x, self.__dh*y, self.__dw, self.__dh)
+                        pygame.draw.rect(self.__screen, TARGET_COLOR, rect)
 
 
 
-        events = pygame.event.get()
-        for event in events:
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_q:
-                    return True
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        return True
 
-        pygame.display.update()
+            pygame.display.update()
 
-a = Simulator((100, 100), 20, 4)
+a = Simulator((100, 100), batch_size=1, agent_size=1, ntargets=3, nwalls=4, rendering=True)
 
 exit = False
 while not exit:
