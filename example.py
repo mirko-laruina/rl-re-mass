d05cfbb6d56f9742c6c6afb860eecbd8238adb11
Mirko <mirkoletto@gmail.com>
2020-02-03 18:32:13 +0100

    Changed trainer and cpu num

--- example.py
+++ example.py
@@ -3,18 +3,20 @@ import time
 import cProfile
 from ray.tune.registry import register_env
 from ray.rllib.agents import ppo
+from ray.rllib.agents.ppo import PPOTrainer
+from ray import tune
 import ray
 
-ray.init()
+ray.init(num_cpus=8)
 register_env("drones_env", lambda c: Simulator((100, 100), batch_size=4,
                                         agent_size=2, ntargets=3,
                                         nwalls=4, observation_range=3,
                                         stig_evaporation_speed=0.5, max_steps=1000,
                                     rendering=False))
-trainer = ppo.PPOAgent(env="drones_env")
-while True:
-    print(trainer.train())
-
+#trainer = ppo.PPOAgent(env="drones_env")
+#while True:
+#    print(trainer.train())
+tune.run(PPOTrainer, config={"env": "drones_env"})
 
 
 import sys
